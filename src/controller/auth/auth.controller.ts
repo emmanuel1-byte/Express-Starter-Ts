4531062f89af069179c5b3a62674c4ee6afbde1b
HilaryEmmanuel <emmanuelhilary9@gmail.com>
2023-11-28 23:14:48 +0100

    initial commit

---
+++ src/controller/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response }  from 'express'
+class Auth {
+    public static async signUp(req: Request, res: Response){
+        try{
+
+        }catch(err){
+            
+        }
+
+    }
+}
