17258066674b050a76578332c08d83c333e13c97
HilaryEmmanuel <emmanuelhilary9@gmail.com>
2023-11-29 12:01:03 +0100

    worked on the server

--- src/index.ts
+++ src/index.ts
@@ -2,39 +2,40 @@ import http from 'http'
 import app from './app'
 
 const server = http.createServer(app)
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
+app.set('port', port)
 
 //Check if port is already in use an if it is exit the process.
-function onError(error: any){
-    switch(error.code){
-        case 'EADDRINUSE':
-          console.clear()
-          console.error('port ' + port + ' is already in use please switch to another port🙏😌')
-          process.exit(1)
-    }
-    }
-    
-    //Event Emmitter that listens to error Event
-    server.on('error', onError)
-    
-    //Database connection
-    async function databaseConnection(){
-        try{
-            //connection
-            console.log('Database connection was established succesfully')
-        }catch(err){
-            console.log('Connection was not established an error occured', err)
-       }
-    }
-    
-    databaseConnection()
-    
-    
-    //Run the server
-    server.listen(port, (()=>{
+function onError(error: NodeJS.ErrnoException) {
+    if (error.code === 'EADDRINUSE') {
         console.clear()
-        console.log(`Express API is running on port ${port} 💪😁....`)
-    })
-     
-    
-    )
+        console.error(` port ${port} is already in use please switch to another port🙏😌`)
+        process.exit(1)
+    } else {
+        console.error('An error occurrred: ', error)
+        process.exit(1)
+    }
+}
+
+
+//Event Emmitter that listens to error Event
+server.on('error', onError)
+
+//Database connection
+async function databaseConnection() {
+    try {
+        //connection
+        console.log('Database connection was established succesfully')
+    } catch (err) {
+        console.log('Connection was not established an error occured', err)
+        process.exit(1)
+    }
+}
+
+
+//Run the server
+server.listen(port, (() => {
+    console.clear()
+    console.log(`Express API is running on port ${port} 💪😁....`)
+    databaseConnection()
+}))
